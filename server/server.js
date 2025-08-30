31837d6b950774a884ba1ba5bed7a4f1e37a0762
yogeshsadgir0005 <yogeshsadgir05@gmail.com>
2025-08-30 11:20:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://earnkaro-flax.vercel.app',
   credentials: true,}));
 
 
